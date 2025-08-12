b50137560fece7066faa70f1f87ec7f08e31f843
Leonardo Henrique Pereira Tubero <106193658+LeonardoHenriqueTubero@users.noreply.github.com>
2025-08-12 11:00:43 -0300

    User & H2 database

---
+++ src/test/java/com/leonardo/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
