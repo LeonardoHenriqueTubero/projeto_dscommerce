26000b5cbcd1369014a24a5478df476b30f925f4
Leonardo Henrique Pereira Tubero <106193658+LeonardoHenriqueTubero@users.noreply.github.com>
2025-08-14 10:42:07 -0300

    OrderItem, OrderItemPK

--- src/main/java/com/leonardo/dscommerce/entities/Order.java
+++ src/main/java/com/leonardo/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.leonardo.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -21,6 +24,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {}
 
@@ -61,5 +67,13 @@ public class Order {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(OrderItem::getProduct).toList();
     }
 }
