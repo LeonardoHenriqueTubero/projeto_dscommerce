7399a9ca1fee6528494b4666e71e24249e20621f
Leonardo Henrique Pereira Tubero <106193658+LeonardoHenriqueTubero@users.noreply.github.com>
2025-08-20 11:08:22 -0300

    Bean Validation

--- src/main/java/com/leonardo/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/leonardo/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.leonardo.dscommerce.controllers.handlers;
 
 import com.leonardo.dscommerce.dto.CustomError;
+import com.leonardo.dscommerce.dto.ValidationError;
 import com.leonardo.dscommerce.services.exceptions.DatabaseException;
 import com.leonardo.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,6 +28,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Datos Inválidos", request.getRequestURI());
+        for(FieldError f: e.getBindingResult().getFieldErrors()) {
+            err.addErrors(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
