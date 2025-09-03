8167b26ed57a217cd5d3a89755c5eb8a178e2bad
Leoanrdo <leonardohenriquetubero@gmail.com>
2025-09-03 11:07:43 -0300

    GET User logged

---
+++ src/main/java/com/leonardo/dscommerce/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.leonardo.dscommerce.controllers;
+
+import com.leonardo.dscommerce.dto.ProductDTO;
+import com.leonardo.dscommerce.dto.UserDTO;
+import com.leonardo.dscommerce.services.ProductService;
+import com.leonardo.dscommerce.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+        UserDTO dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+}
